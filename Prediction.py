d51b5dac267186654b6359d3e99e57edc0823a24
jessychen613 <83385232+jessychen613@users.noreply.github.com>
2021-06-03 18:47:29 -0700

    Add files via upload

--- Prediction.py
+++ Prediction.py
@@ -5,6 +5,7 @@ from PIL import Image
 import numpy as np
 import os.path
 from ReadTFRecord import load_dataset
+from ReadTFRecord import load_datasetforpredict
 from keras import models
 import matplotlib.pyplot as plt
 import tensorflow as tf
@@ -21,9 +22,10 @@ model.summary()
 
 model.load_weights('ckpt.h5')
 
-data, labels, testvideo, testlabel = load_dataset(datasetpath1)
+#data, labels, testvideo, testlabel = load_dataset(datasetpath1)
+data, labels = load_datasetforpredict(datasetpath1)
 print("label", labels)
-print("data", data)
+#print("data", data)
 
 samples_to_predict = []
 
@@ -38,6 +40,8 @@ samples_to_predict = []
 
 #predict2 = model.predict(np.array(data))
 predictclass = []
+zeronum = 0
+onenum = 0
 
 for example in data:
 
@@ -55,8 +59,15 @@ for example in data:
 #    print("predict2",predict2)
     classes = np.argmax(predict2, axis = 1)
 #    print("classes",classes)
+
+    if (classes[0]==0):
+        zeronum = zeronum + 1
+    if (classes[0]==1):
+        onenum = onenum + 1
     predictclass.append(classes[0])
 
 #    print("predict2",predict2)
 
 print("predictclass", predictclass)
+print("zeronum", zeronum)
+print("onenum", onenum)
--- ReadTFRecord.py
+++ ReadTFRecord.py
@@ -124,6 +124,82 @@ def load_dataset(filepath):
 
     return train_addrs, train_labels, test_addrs, test_labels
 
+def load_datasetforpredict(filepath):
+
+    filenames = gfile.Glob(os.path.join(filepath, file_suffix))
+    filenum = len(filenames)
+
+    print('Total videos found: ' + str(filenum))
+    print(filenames)
+    print(filenames[0])
+
+    num_frame = 0
+    video_train = []
+ #  video_test = np.zeros((124,240,320,3))
+    label_train = []
+
+    for file_i in range(filenum):
+        print("file loop",file_i)
+        filepath = filenames[file_i]
+        print("filepath",filepath)
+
+        raw_image_dataset = tf.data.TFRecordDataset(filepath)
+        print("before parsed_image_dataset")
+        parsed_image_dataset = raw_image_dataset.map(_parse_image_function)
+
+        print("after parsed_image_dataset")
+
+        i=num_frame
+        for image_features in parsed_image_dataset:
+#            num_images = image_features['num_images'].numpy()
+#            height = image_features['height'].numpy()
+#            width = image_features['width'].numpy()
+#            channel = image_features['channel'].numpy()
+#           print(num_images,height,width,channel)
+            video = tf.io.decode_raw(image_features['video'], tf.uint8)
+            video = tf.reshape(video, [HEIGHT, WIDTH, CHANNEL])
+#            plt.imshow(video)
+#            plt.show()
+#            video = video[:, 20:280, :]
+#            plt.imshow(video)
+#            plt.show()
+#            print("videotest", video.shape)
+#            print("before video_train.append")
+#            tf.image.convert_image_dtype(video, dtype=tf.float32)
+            video_train.append(tf.cast(video, tf.float32) / 255.0)
+#            print("after video_train.append")
+#           display(Image(data=video))
+            label = image_features['label'].numpy()
+#            if file_i==1:
+#                print(video)
+#                print(label)
+            label_train.append(tf.cast(label, tf.float32))
+#           label_train[i] = label
+            i=i+1
+#            print(label_train)
+
+        num_frame = i
+        file_i=file_i+1
+ #   video_traint = tf.stack(video_train)
+ #   label_traint = tf.stack(label_train)
+
+    video_trainl = np.array(video_train)
+    label_trainl = np.array(label_train)
+
+    print("video_train shape",video_trainl.shape)
+#    print("video_train",video_trainl)
+#    print("label_train",label_trainl)
+    print("label_train shape",label_trainl.shape)
+
+    c = list(zip(video_trainl, label_trainl))
+#    shuffle(c)
+    addrs, labels = zip(*c)
+
+    train_addrs = np.array(addrs)
+    train_labels = np.array(labels)
+
+    return train_addrs, train_labels
+
 #video_train, label_train, video_test, label_test = load_dataset(filepathtest)
 #print("video_train shape",video_train.shape)
 #print("video_train",video_train)
